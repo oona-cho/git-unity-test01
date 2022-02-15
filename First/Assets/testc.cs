9ceddba541fa844d7a0ed12f184ffcdbdaf92820
oona-cho <99732995+oona-cho@users.noreply.github.com>
2022-02-15 22:22:09 +0900

    1

---
+++ First/Assets/testc.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testc : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       // print("Hello World"); 
+      
+      //int num;
+      //num = 10;
+      //print(num);
+
+      //int num = 0;
+      //int value1 = 2; 
+      //num = value1; 
+      //print(num);
+      //int value2 = 10;
+      //num = value2;
+      //print(num);
+
+      //int a; 
+      //a = 2;
+      //int b;
+      //b = 5;
+      //int temp; 
+      //temp = a;
+      //a = b;
+      //b = temp;
+      //print("a값 :" + a);
+      //print("b값 :" + b);
+
+      //int num1,num2,result; 
+      float num1,num2,result; 
+      num1 = 10;
+      num2 = 3; 
+      result = num1 + num2;
+      print(result);
+      result = num1 - num2;
+      print(result);
+      result = num1 * num2;
+      print(result);
+      result = num1 / num2;
+      print(result);
+      result = num1 % num2;
+      print(result);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
